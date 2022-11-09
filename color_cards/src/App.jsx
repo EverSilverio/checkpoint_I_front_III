11f3cd99f306dc71a5e4e0670aed6ce62aa3a54c
EverSilverio <ever.silverio@gmail.com>
2022-11-09 18:00:35 -0300

    create setCards

--- color_cards/src/App.jsx
+++ color_cards/src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './App.css'
 import Card from './components/Card'
 
 const App = () => {
+   /* state form */
+   const [form, setForm] = useState({
+      colorName: '',
+      colorCode: ''
+   })
+
+   /* state lista de cards */
+   const [cards, setCards] = useState([])
+
    function onSubmit(e) {
       e.preventDefault();
 
-      alert('opa')
+      console.log(cards);
+      console.log(form);
+
+      setCards([...cards, form ])
+
    }
 
 
@@ -13,11 +27,18 @@ const App = () => {
       <>
          <h1>Initial</h1>
          <form onSubmit={onSubmit}>
-            <input />
-            <input />
+            <input value={form.colorName} onChange={ e => setForm({ ...form, colorName:e.target.value })}/>
+            <input value={form.colorCode} onChange={ e => setForm({ ...form, colorCode:e.target.value })}/>
             <input type="submit" value="Salvar" />
          </form>
          <Card data="novo card"></Card>
+
+         <h2>FORM</h2>
+         <span>{JSON.stringify(form)}</span>
+
+         <h2>CARDS</h2>
+         <span>{JSON.stringify(cards)}</span>
+
       </>
 
    )
